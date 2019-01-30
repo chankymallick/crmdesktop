63b3b2be127487f4af068f6f5e522247a417dfbe
chanky <chanky.mallick@gmail.com>
2019-01-30 11:28:38 +0530

    Enh off

---
+++ src/app/TemporaryData.js
@@ -0,0 +1,45 @@
+// {
+//     "leadUid":6,
+//     "customerUid": 1001,
+//     "description": "Found In Facebook",
+//     "leadSource": "Facebook",
+//     "status": "RAW_LEAD",
+//     "rating": 3,
+//     "createdBy": 101,
+//     "createdDate": "2019-01-28T18:30:00.000+0000",
+//     "modifiedBy": 101,
+//     "modifiedDate": "2019-01-28T18:30:00.000+0000",
+//     "note": 1
+//     }
+//     {
+    
+//     "firstname": "ayesa",
+//     "lastname": "MALLICK",
+//     "customerStage": "LEAD",
+//     "assignedTo": 1001,
+//     "country": "INDIA",
+//     "currency": "SGD",
+//     "revenuePotential": 3,
+//     "serviceInterested": "FOREX",
+//     "description": "GOT FROM FB",
+//     "createdBy": 1001,
+    
+//     "modifiedBy": 1001
+    
+//     }
+//     {
+//     "customerId": 1001,
+//     "callType": "DONE",
+//     "callDetails": "schedule call",
+//     "callTime": "2019-01-23T18:22:15.767+0000",
+//     "callDurationMinute": 3,
+//     "callDurationSecond": 10,
+//     "callResult": "RE_CALL",
+//     "description": "OK",
+//     "note": null,
+//     "createdBy": 44,
+//     "createdDate": "1548476604426",
+//     "modifiedBy": 122,
+//     "modifiedDate": "1548476604426"
+//     }
+    
